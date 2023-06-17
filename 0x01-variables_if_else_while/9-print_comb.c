8d331e36abe67e6e150d2378baac698d4ed15857
redondo2k <redaothmane2016@gmail.com>
2023-06-17 07:46:25 -0700

    w

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lower then in upper case
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		puchar(n + 48);
+
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
