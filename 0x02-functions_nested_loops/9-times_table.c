152feffd9c671302f721161611a96f07bc18ec98
redondo2k <redaothmane2016@gmail.com>
2023-06-20 07:35:01 -0700

    w

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (num = 1; num <= 9; mlt++)
+		for (mlt = 1; mlt <= 9; mlt++)
 		{
 			_putchar(',');
 			_putchar(' ');
