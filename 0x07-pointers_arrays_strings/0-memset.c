5b9336e257c5b3218769930a01b8757e9e6b638f
redondo2k <redaothmane2016@gmail.com>
2023-07-03 14:53:01 -0700

    w

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte 
+ * @s: pointer to put the const
+ * @b: const
+ * @n: max bytes 
+ * Return: pointer a 
+ */
+
+char char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int t;
+
+	for (t = 0; n > 0; t++, n--)
+	{
+		s[t] = b;
+	}
+	
+	return (s);
+}
