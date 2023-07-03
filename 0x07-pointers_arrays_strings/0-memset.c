025629426f669936e4a425ddc9feebf24d272d79
redondo2k <redaothmane2016@gmail.com>
2023-07-03 15:04:38 -0700

    w

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,14 +8,14 @@
  * Return: pointer s
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int t;
 
 	for (t = 0; n > 0; t++; n--)
 	{
 		s[t] = b;
-	}
+	}i
 
 	return (s);
 }
