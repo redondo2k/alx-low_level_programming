16534ae861721c1a3cfe419bb1663e6e05b9b05d
redondo2k <redaothmane2016@gmail.com>
2023-07-07 05:15:01 -0700

    w

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int i, least = 0, money = aoi(argv[1]);
+		int i, least = 0, money = atoi(argv[1]);
 		int cents[] = {25, 10, 5, 2, 1};
 
 		for (i = 0; i < 5; i++)
