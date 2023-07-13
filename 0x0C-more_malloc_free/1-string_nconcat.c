ea484bd701e4722940b280ba5fe2866b89ed7e43
redondo2k <redaothmane2016@gmail.com>
2023-07-13 13:10:23 -0700

    w

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-	s2 = "";
+		s2 = "";
 	/*compute the lenght of the string*/
 
 	for (s11 = 0; s1[s11] != '\0'; s11++)
