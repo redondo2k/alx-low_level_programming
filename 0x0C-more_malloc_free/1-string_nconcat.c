0f86c89a68b4ac36a6769670c784213be9881b46
redondo2k <redaothmane2016@gmail.com>
2023-07-13 13:19:00 -0700

    w

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,10 +35,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	/*copy first string into str*/
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
-	/*copy the second one*/
+	/*copy the second into str*/
 	for (j = 0; j < n; j++)
 	{
-		str[i] = s2[i];
+		str[i] = s2[j];
 		i++;
 	}
 
