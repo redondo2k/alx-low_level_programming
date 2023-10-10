0235a08d616c7101171fde3ca3e5621534b56df1
redondo2k <redaothmane2016@gmail.com>
2023-10-10 12:06:28 -0700

    w

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struc dog - a dog struct
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: owners name
+ *
+ * Description: just a lone dog struct in a big kitty world
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
