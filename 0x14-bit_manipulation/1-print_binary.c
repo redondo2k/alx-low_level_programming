a802d86064d6135a53b7eb0751a6552a4ed1df48
redondo2k <redaothmane2016@gmail.com>
2023-10-26 15:08:59 -0700

    w

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a num as binary string
+ * @n: the num
+ * Retunr: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8, printed = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			printed++;
+		}
+		else if (printed)
+			_putchar('0');
+	}
+	if (!printed)
+		_putchar('0');
+}
